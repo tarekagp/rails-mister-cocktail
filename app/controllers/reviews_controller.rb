46f13091d157eca2d7be3e2556d57e7071cc08ba
Tarek Plaza <tarek.agp@gmail.com>
2018-08-03 16:53:21 +0100

    basic version

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,25 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @dose = Dose.new
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_110844) do
+ActiveRecord::Schema.define(version: 2018_08_03_141329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 2018_08_03_110844) do
     t.string "name"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "cocktail_id"
+    t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
+  end
+
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
+  add_foreign_key "reviews", "cocktails"
 end
