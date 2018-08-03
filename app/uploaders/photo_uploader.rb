b7f8b4b89fca55319e444732d9a846fae32af96c
Tarek Plaza <tarek.agp@gmail.com>
2018-08-03 14:43:17 +0100

    revised ii)

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,15 +1,14 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
   process eager: true  # Force version generation at upload time.
+  # process convert: 'jpg'
 
-  process convert: 'jpg'
+  # version :thumnail do
+  #   resize_to_fit 256, 256
+  # end
 
-  version :thumnail do
-    resize_to_fit 256, 256
-  end
-
-  version :bright_face do
-    cloudinary_transformation effect: "brightness:30", radius: 20,
-      width: 150, height: 150, crop: :thumb, gravity: :face
-  end
+  # version :bright_face do
+  #   cloudinary_transformation effect: "brightness:30", radius: 20,
+  #     width: 150, height: 150, crop: :thumb, gravity: :face
+  # end
 end
